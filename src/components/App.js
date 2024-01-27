564f3ff76ff30c2bbca54425f7b83f97435f58d5
gajendra <gajendrayadav15798@gmail.com>
2024-01-27 20:47:50 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import "../styles/App.css";
+import Album from "./Album";
+import Login from "./Login";
+import Signup from "./Signup";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="App">
+  <Router>
+  <Routes>
+  <Route path="/" element={<Album/>}/> 
+  <Route  path="/Login" element={<Login />}/>
+  <Route  path="/Signup" element={<Signup />}/>
+  </Routes>
+  </Router>
+  </div>
+ 
 }
 
 export default App;
