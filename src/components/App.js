81a5eaaf4e56c28165c67069491c5f16d57be2fb
gajendra <gajendrayadav15798@gmail.com>
2024-01-29 20:44:53 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,14 @@ import Login from "./Login";
 import Signup from "./Signup";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
+import axios from "axios";
 
 
 function App() {
+  axios.interceptors.request.use(async (config) => {
+    config.headers['projectid'] = "f104bi07c490";
+    return config;
+  })
   return <div className="App">
   <Router>
   <Navbar/>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from 'axios';
 
 function Signup() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password:"",
+    appType:"music"
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('https://academics.newtonschool.co/api/v1/user/signup', data)
+    .then((response)=>{
+      console.log(response)
+    }).catch((error)=>{
+      console.log(error);
+    });
+    console.log(data);
+  };
   return (
     <div>
-      <h1> Signup </h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label htmlFor="appType">App Type</label>
+        <select
+          name="appType"
+          onChange={handleChange}
+          id="appType"
+          required
+        >
+          <option value="music">music</option>
+          <option value="album">album</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
-
-export default Signup
+export default Signup;
